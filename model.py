deb640ede25999829140f17191ee3df003730204
Changyu Liu <shiyipaisizuo@163.com>
2019-09-02 22:38:51 +0800

    Update model.py

--- model.py
+++ model.py
@@ -23,7 +23,7 @@ class AlexNet(nn.Module):
   def __init__(self):
     super(AlexNet, self).__init__()
     self.features = nn.Sequential(
-      nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=2),
+      nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2),
       nn.ReLU(inplace=True),
       nn.MaxPool2d(kernel_size=2, stride=2),
       nn.Conv2d(64, 192, kernel_size=3, stride=1, padding=1),
