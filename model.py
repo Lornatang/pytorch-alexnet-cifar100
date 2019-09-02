2771f99c475459d92f9aed4ca154d1423da5d0de
Changyu Liu <shiyipaisizuo@163.com>
2019-09-02 16:52:21 +0800

    Update the code to Torch1.0

--- model.py
+++ model.py
@@ -1,37 +1,62 @@
-from torch import nn
+# Copyright 2019 Lorna Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import torch
+import torch.nn as nn
+
+__all__ = ['AlexNet', 'alexnet']
 
 
 class AlexNet(nn.Module):
 
-    def __init__(self, num_classes=100):
-        super(AlexNet, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(64, 192, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2)
-        )
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 2 * 2, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
+  def __init__(self):
+    super(AlexNet, self).__init__()
+    self.features = nn.Sequential(
+      nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=2),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(kernel_size=2, stride=2),
+      nn.Conv2d(64, 192, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(kernel_size=2, stride=2),
+      nn.Conv2d(192, 384, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(inplace=True),
+      nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(inplace=True),
+      nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(kernel_size=2, stride=2),
+    )
+    self.classifier = nn.Sequential(
+      nn.Dropout(),
+      nn.Linear(256 * 3 * 3, 4096),
+      nn.ReLU(inplace=True),
+      nn.Dropout(),
+      nn.Linear(4096, 4096),
+      nn.ReLU(inplace=True),
+      nn.Linear(4096, 10),
+    )
 
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), 256 * 2 * 2)
-        x = self.classifier(x)
-        return x
+  def forward(self, x):
+    x = self.features(x)
+    x = torch.flatten(x, 1)
+    x = self.classifier(x)
+    return x
+
+
+def alexnet(**kwargs):
+  r"""AlexNet model architecture from the
+  `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+  """
+  model = AlexNet(**kwargs)
+  return model
