33da33cee72a06dd1877e418b54c20c549dd19b4
Changyu Liu <shiyipaisizuo@163.com>
2019-09-04 10:29:49 +0800

    Update model.py

--- model.py
+++ model.py
@@ -23,23 +23,23 @@ class AlexNet(nn.Module):
   def __init__(self, classes=100):
     super(AlexNet, self).__init__()
     self.features = nn.Sequential(
-      nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2),
+      nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
       nn.ReLU(inplace=True),
-      nn.MaxPool2d(kernel_size=2, stride=2),
+      nn.MaxPool2d(kernel_size=3, stride=2),
       nn.Conv2d(64, 192, kernel_size=3, stride=1, padding=1),
       nn.ReLU(inplace=True),
-      nn.MaxPool2d(kernel_size=2, stride=2),
+      nn.MaxPool2d(kernel_size=3, stride=2),
       nn.Conv2d(192, 384, kernel_size=3, stride=1, padding=1),
       nn.ReLU(inplace=True),
       nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
       nn.ReLU(inplace=True),
       nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
       nn.ReLU(inplace=True),
-      nn.MaxPool2d(kernel_size=2, stride=2),
+      nn.MaxPool2d(kernel_size=3, stride=2),
     )
     self.classifier = nn.Sequential(
       nn.Dropout(),
-      nn.Linear(256 * 3 * 3, 4096),
+      nn.Linear(256 * 1 * 1, 4096),
       nn.ReLU(inplace=True),
       nn.Dropout(),
       nn.Linear(4096, 4096),
